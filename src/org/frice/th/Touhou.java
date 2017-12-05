1bc51c65f260658b7fe8340e0a4f479ce06fd621
ice1000 <ice1000kotlin@foxmail.com>
2017-12-05 16:46:55 +0800

    [ bullet ] player bullet

--- src/org/frice/th/Touhou.java
+++ src/org/frice/th/Touhou.java
@@ -3,12 +3,15 @@ package org.frice.th;
 import org.frice.Game;
 import org.frice.Initializer;
 import org.frice.anim.move.CustomMove;
+import org.frice.anim.move.SimpleMove;
 import org.frice.obj.sub.ImageObject;
 import org.frice.obj.sub.ShapeObject;
 import org.frice.resource.graphics.ColorResource;
 import org.frice.resource.image.FileImageResource;
 import org.frice.resource.image.FrameImageResource;
 import org.frice.resource.image.ImageResource;
+import org.frice.utils.BoolArray;
+import org.frice.utils.message.FLog;
 import org.frice.utils.shape.FRectangle;
 import org.frice.utils.time.FTimer;
 import org.jetbrains.annotations.NotNull;
@@ -20,13 +23,13 @@ import java.util.stream.IntStream;
 
 public class Touhou extends Game {
 	private int backgroundSpeed = 2;
-	private final int backgroundPicCountX = 3;
-	private final int backgroundPicCountY = 3;
-	private int direction = 0;
-	private int pressedKeyCount = 0;
-	private int speed = 6;
+	private static final int backgroundPicCountX = 3;
+	private static final int backgroundPicCountY = 3;
+	private static final int fastSpeed = 5;
+	private BoolArray direction = new BoolArray(5);
+	private int speed = fastSpeed;
 	private ImageObject player = player();
-	private FTimer moveTimer = new FTimer(6);
+	private FTimer moveTimer = new FTimer(10);
 
 	public Touhou() {
 		// super(640, 480, 2);
@@ -36,28 +39,44 @@ public class Touhou extends Game {
 	@Override
 	public void onInit() {
 		setSize(640, 480);
-		setAutoGC(false);
+		setAutoGC(true);
+		setShowFPS(false);
 		setMillisToRefresh(10);
+		FLog.INSTANCE.setLevel(FLog.ERROR);
 		Consumer<KeyEvent> a = e -> {
-			direction = e.getKeyCode();
-			if (e.getKeyCode() >= KeyEvent.VK_LEFT && e.getKeyCode() <= KeyEvent.VK_DOWN) pressedKeyCount++;
-			speed = e.isShiftDown() ? 1 : 6;
+			if (e.getKeyCode() >= KeyEvent.VK_LEFT && e.getKeyCode() <= KeyEvent.VK_DOWN)
+				direction.set(e.getKeyCode() - KeyEvent.VK_LEFT, true);
+			speed = e.isShiftDown() ? 1 : 3;
+			if (e.getKeyCode() == KeyEvent.VK_Z) direction.set(4, true);
 		};
 		addKeyListener(a, a, e -> {
-			if (e.getKeyCode() >= KeyEvent.VK_LEFT && e.getKeyCode() <= KeyEvent.VK_DOWN) pressedKeyCount--;
-			if (pressedKeyCount == 0) direction = 0;
+			if (e.getKeyCode() >= KeyEvent.VK_LEFT && e.getKeyCode() <= KeyEvent.VK_DOWN)
+				direction.set(e.getKeyCode() - KeyEvent.VK_LEFT, false);
+			if (e.getKeyCode() == KeyEvent.VK_Z) direction.set(4, false);
 		});
 	}
 
 	@Override
 	public void onRefresh() {
 		if (moveTimer.ended()) {
-			if (direction == KeyEvent.VK_LEFT && player.getX() > 10) player.setX(player.getX() - speed);
-			else if (direction == KeyEvent.VK_RIGHT && player.getX() < 300) player.setX(player.getX() + speed);
-			else if (direction == KeyEvent.VK_UP && player.getY() > 10) player.setY(player.getY() - speed);
-			else if (direction == KeyEvent.VK_DOWN && player.getY() < getHeight() - player.getHeight() - 10)
+			if (direction.get(0) && player.getX() > 10) player.setX(player.getX() - speed);
+			if (direction.get(KeyEvent.VK_RIGHT - KeyEvent.VK_LEFT) && player.getX() < 300)
+				player.setX(player.getX() + speed);
+			if (direction.get(KeyEvent.VK_UP - KeyEvent.VK_LEFT) && player.getY() > 10) player.setY(player.getY() - speed);
+			if (direction.get(KeyEvent.VK_DOWN - KeyEvent.VK_LEFT) && player.getY() < getHeight() - player.getHeight() - 10)
 				player.setY(player.getY() + speed);
+			if (direction.get(4)) {
+				addObject(1, bullet());
+			}
 		}
+	}
+
+	private ImageObject bullet() {
+		ImageResource bullet = new FileImageResource("./res/th11/player/pl01/pl01.png").part(16, 160, 16, 16);
+		ImageObject object = new ImageObject(bullet, player.getX() + (player.getWidth() - bullet.getImage().getWidth()) / 2, player.getY());
+		// object.addAnim(new RotateAnim(10));
+		object.addAnim(new SimpleMove(0, -900));
+		return object;
 	}
 
 	@Override
@@ -69,13 +88,13 @@ public class Touhou extends Game {
 
 	@NotNull
 	private ImageObject player() {
-		ImageResource bigImage = new FileImageResource("./res/th11/player/pl00/pl00.png");
+		ImageResource bigImage = new FileImageResource("./res/th11/player/pl01/pl01.png");
 		return new ImageObject(new FrameImageResource(IntStream.range(0, 8)
 				.mapToObj(x -> bigImage.part(x * 32, 0, 32, 48)).collect(Collectors.toList()), 50), 0, 0);
 	}
 
 	private void background() {
-		ImageResource image = new FileImageResource("./res/th11/background/stage04/stage04a.png");
+		ImageResource image = new FileImageResource("./res/th11/background/stage04/stage04c.png");
 		for (int x = 0; x < backgroundPicCountX; x++)
 			for (int y = 0; y < backgroundPicCountY; y++) {
 				ImageObject object = new ImageObject(image, x * image.getImage().getWidth(), y * image.getImage().getHeight());
